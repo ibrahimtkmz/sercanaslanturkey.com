a5755bd47d5a6d3571bc606654d69bc41aaca8a0
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-27 11:47:06 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
